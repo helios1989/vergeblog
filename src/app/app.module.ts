715a8195f8d1429969d313dee2bb97c9e82fd557
Barit <VNBarit@rcgitmanila.com>
2017-06-14 03:11:39 +0800

    added hader and homepage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { BlogDetailsComponent } from './blogs/blog-details/blog-details.component';
 import { BlogListComponent } from './blogs/blog-list/blog-list.component';
+import { HeaderComponent } from './header/header.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     BlogDetailsComponent,
-    BlogListComponent
+    BlogListComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
