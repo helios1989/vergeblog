5931501e3d7c19763e3d6d3bc1a63b28b06f9897
Barit <VNBarit@rcgitmanila.com>
2017-06-14 04:15:15 +0800

    added components

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { BlogDetailsComponent } from './blogs/blog-details/blog-details.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { BlogListComponent } from './blogs/blog-list/blog-list.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: BlogListComponent },
+  { path: 'about', component:  AboutComponent },
+  { path: 'contact-us', component: ContactUsComponent },
+  { path: 'blog/:id', component: BlogDetailsComponent },
+  { path: '404', component: NotFoundComponent },
+  { path: '**', redirectTo: '/404' }
+];
+
+@NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [RouterModule]
+})
+export  class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,19 +7,26 @@ import { AppComponent } from './app.component';
 import { BlogDetailsComponent } from './blogs/blog-details/blog-details.component';
 import { BlogListComponent } from './blogs/blog-list/blog-list.component';
 import { HeaderComponent } from './header/header.component';
-
+import { AboutComponent } from './about/about.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { AppRoutingModule }     from './app-routing.module';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BlogDetailsComponent,
     BlogListComponent,
-    HeaderComponent
+    HeaderComponent,
+    AboutComponent,
+    ContactUsComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
     HttpModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
