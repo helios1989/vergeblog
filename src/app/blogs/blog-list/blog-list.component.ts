975ab170fa782d5c728937f791ee378864900e8a
Vergel <vergel@mokucollective.com>
2017-06-15 12:19:35 +0800

    added placeholder for udpate and delete

--- src/app/blogs/blog-list/blog-list.component.ts
+++ src/app/blogs/blog-list/blog-list.component.ts
@@ -22,7 +22,7 @@ export class BlogListComponent implements OnInit {
     // const raw = '2017-06-13T05:36:22.474Z';
     // this.testDate = moment(raw).format('MMMM do YYYY h:mm:ss a');
     this.blogService.getBlogs()
-      .then((blogs: Blog[]) => {
+      .then((blogs: Blog[]) => {  
         if (blogs) {
         this.blogs = blogs.map((blogs) => {
           if(!blogs.title) {
@@ -39,5 +39,13 @@ export class BlogListComponent implements OnInit {
     //add new blog
     this.router.navigate(['blog', 0]);
     return false;
+  }
+
+  updateBlog(id: number) {
+    alert(id);
+  }
+
+  deleteBlog(id: number) {
+    alert(id);
   }
 }
