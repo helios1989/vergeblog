ba414f90629d1415c1b8bee49acb852ed3617692
Barit <VNBarit@rcgitmanila.com>
2017-06-16 03:07:22 +0800

    remove logger

--- server.js
+++ server.js
@@ -64,7 +64,8 @@ app.get("/api/blogs/:id", function(req, res) {
 
 
 app.delete("/api/blogs/:id", function(req, res) {
-  console.log(req.params);
+  // console.log(req.params);
+  console.log('whats');
   db.collection(COLLECTION_NAME).deleteOne({_id: new ObjectID(req.params.id)}, function(err, result) {
 
     if (err) {
