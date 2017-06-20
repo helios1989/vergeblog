2b3cfe603fd32f71a66b26b67272d8e394375432
Barit <VNBarit@rcgitmanila.com>
2017-06-21 00:10:40 +0800

    added dev package

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ var port = process.env.PORT || 8080;
 var app = express();
 
 var twilio = require('twilio');
+var client = new twilio.RestClient('AC9b37a72f5e09062e3e6fd289a5c1e706', 'b3062f19ca21ef2b8ddf9885fbc93a0b');
 app.use(bodyParser.json());
 //create link to angular build directory
 var distDir = __dirname + "/dist";
@@ -99,21 +100,20 @@ app.post("/api/blogs", function(req, res) {
   });
 });
 app.get("/api/sendText/:message", function(req, res){
-  // var client = new twilio.RestClient('AC9b37a72f5e09062e3e6fd289a5c1e706', 'b3062f19ca21ef2b8ddf9885fbc93a0b');
-  res.status(200).json(req.params.message + 'tesss');
-  // Pass in parameters to the REST API using an object literal notation. The
-  // REST client will handle authentication and response serialzation for you.
-  // client.sms.messages.create({
-  //     to:'+63926804907',
-  //     from:'+13342199006',
-  //     body:'ahoy hoy! Testing Twilio and node.js'
-  // }, function(error, message) {
-  //     if (!error) {
-  //         console.log('Success! The SID for this SMS message is:');
+  //
 
-  //     } else {
-  //         console.log('Oops! There was an error.');
-  //     }
-  // });
+
+  client.sms.messages.create({
+      to:'+63926804907',
+      from:'+13342199006',
+      body:'ahoy hoy! Testing Twilio and node.js'
+  }, function(error, message) {
+      if (!error) {
+          console.log('Success! The SID for this SMS message is:');
+          res.status(200).json(req.params.message + 'tesss');
+      } else {
+          console.log('Oops! There was an error.');
+      }
+  });
 
 });
